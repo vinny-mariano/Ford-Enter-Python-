a64780d2eb18d76a36b28c29422c08bae8bc4e34
Vinicius <vinicius.m.alves8@edu.senai.br>
2026-03-24 09:00:55 -0300

    Reorganizando pastas

---
+++ 24.03/comandos_git.py
@@ -0,0 +1,12 @@
+"""
+git add nome_do_arquivo
+git add . (adiciona todos os arquivos na pasta)
+
+git commit -m "Seu comentário" (adiciona o "log"
+de alterações com comentário)
+
+git push -u origin ("empurra" os arquivos para o
+repositório)
+
+
+"""
